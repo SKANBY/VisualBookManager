2ac5fe8e57489204151be8316bd0e6539b7d357e
김나영 <KNY@gimnayeongui-MacBook-Pro.local>
2017-12-11 22:39:14 +0900

    전체보기 버튼 기능 생성

--- VisualBookManager/ViewController.swift
+++ VisualBookManager/ViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let myBook = BookManager()  //전역변수
+    @IBOutlet var outputTextView:UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -18,23 +20,16 @@ class ViewController: UIViewController {
         book1.genre = "비극"
         book1.author = "셰익스피어"
         
-        //book1.bookPrint()
-        
         let book2 = Book()
         book2.name = "누구를 위하여 종을 울리나"
         book2.genre = "전쟁소설"
         book2.author = "헤밍웨이"
         
-        //book2.bookPrint()
-        
         let book3 = Book()
-        book3.name = "죄와벌"
+        book3.name = "죄와 벌"
         book3.genre = "사실주의"
         book3.author = "톨스토이"
         
-        //book3.bookPrint()
-        
-        let myBook = BookManager()
         
         myBook.addBook(book1)
         myBook.addBook(book2)
@@ -42,6 +37,10 @@ class ViewController: UIViewController {
         
     }
 
+    @IBAction func showAllBookAction(_ sender:AnyObject){   //IB = Interface Builder
+        //print(myBook.showAllBook())
+        outputTextView.text = myBook.showAllBook()
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
